469e1391a3514d35cd546b2100dfad770253ef16
Yufei Ou <yo2265@columbia.edu>
2016-10-11 23:48:15 -0400

    fix wrap around bug

--- Player.java
+++ Player.java
@@ -10,33 +10,33 @@ public class Player implements slather.sim.Player {
 
 	private Random gen;
 	// weight parameters
-	private final static int NUMBER_OF_RANDOM_TRY = 4;
+	private final static int NUMBER_OF_RANDOM_TRY = 100;
 	private final static double PHEROME_IMPORTANCE = 0.2;
-	// angel
-	private final static int ANGEL_RANGE = 360;
-	private final static int SCALE = 3; // establish mapping from ANGEL_RANGE to byte so that every arg is in range [0, 120)
-	private int our_angel_range;
+
+	// range for tail length
+	private final static int LOWEST_LEN_OF_TAIL = 10;
+
+	// angle
+	private final static int ANGLE_RANGE = 360;
+	private final static int SCALE = 3; // establish mapping from ANGLE_RANGE to byte so that every arg is in range [0, 120)
+	private int our_angle_range;
+	
+	// input variable
 	private int tail;
 	private double visible_distance;
+	private double side;
 
 	public void init(double d, int t, int side_length) {
 		this.gen = new Random();
 		this.visible_distance = d;
 		this.tail = t;
-		this.our_angel_range = Player.ANGEL_RANGE / Player.SCALE;
-	}
-
-	private boolean f(Cell player_cell, Set<Cell> nearby_cells) {
-		if (nearby_cells.size() != 1) return false;
-		for (Cell nearby_cell : nearby_cells) {
-			return nearby_cell.player == player_cell.player;
-		}
-		return true;
+		this.side = side_length;
+		this.our_angle_range = Player.ANGLE_RANGE / Player.SCALE;
 	}
 
 	public Move play(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
 		if (player_cell.getDiameter() >= 2) // reproduce whenever possible
-			return new Move(true, (byte) -1, (byte) -1);
+			return new Move(true, (byte) -128, (byte) -128);
 
 		// care bout your children, dude!
 
@@ -44,17 +44,46 @@ public class Player implements slather.sim.Player {
 		int arg = 0;
 		int spin_sep = get_spin_sep(this.tail, this.visible_distance); 
 		int detector_sep = get_detector_sep(this.tail, this.visible_distance);
-		if (memory < 0 && nearby_cells.size() > 0) {
-			//System.out.println("here");
-			arg = getOppositeDirection(player_cell, nearby_cells, memory);
-			// System.out.println(arg);
-		} else if (nearby_cells.size() == 0 ) {// f(player_cell, nearby_cells) ) {
+
+
+		if (memory == -128 && nearby_cells.size() > 0) {
+			arg = getOppositeDirection(player_cell, nearby_cells);
+		} else if (nearby_cells.size() == 0) {
 			arg = memory;
 		} else if (isCrowded(player_cell, nearby_cells, nearby_pheromes, 2) == true) {
-			arg = spin(player_cell, memory, nearby_cells, nearby_pheromes, spin_sep);
+
+			int tmp_arg = 0;
+			if (memory < 0 && memory >= -120) {// spin in opposite direction
+				tmp_arg = spin(player_cell, (byte)(-memory - 1) , nearby_cells, nearby_pheromes, spin_sep, false);
+				//arg = spin(player_cell, -memory+1, nearby_cells, nearby_pheromes, spin_sep, false);
+			} else {
+				tmp_arg = spin(player_cell, memory, nearby_cells, nearby_pheromes, spin_sep, true);
+				//arg = spin(player_cell, memory, nearby_cells, nearby_pheromes, spin_sep, true);
+			}
+			
+			Point tmp_vector = extractVectorFromAngle(tmp_arg);
+
+			if (!collides(player_cell, tmp_vector, nearby_cells, nearby_pheromes)) {
+				if (memory < 0 && memory >= -120) {
+					return new Move(tmp_vector, (byte) (-tmp_arg - 1));
+				} else {
+					return new Move(tmp_vector, (byte) tmp_arg);	
+				}
+				
+			} else {
+				arg = (memory + 60) % 120;
+				tmp_vector = extractVectorFromAngle(arg);
+				if (!collides(player_cell, tmp_vector, nearby_cells, nearby_pheromes)) {
+					
+					return new Move(tmp_vector, (byte) (-arg-1));
+				}
+
+			}
+
 		} else {
 			arg = detector(player_cell, memory, nearby_cells, nearby_pheromes, detector_sep);
 		}
+
 
 		Point vector = extractVectorFromAngle(arg);
 		if (!collides(player_cell, vector, nearby_cells, nearby_pheromes)) {
@@ -63,7 +92,7 @@ public class Player implements slather.sim.Player {
 
 		// backup strategy: random escape
 		for (int i = 0; i < Player.NUMBER_OF_RANDOM_TRY; i++) {
-			arg = gen.nextInt(this.our_angel_range) + 1;
+			arg = gen.nextInt(this.our_angle_range) + 1;
 			vector = extractVectorFromAngle(arg);
 			if (!collides(player_cell, vector, nearby_cells, nearby_pheromes))
 				return new Move(vector, (byte) arg);
@@ -71,19 +100,18 @@ public class Player implements slather.sim.Player {
 		return new Move(new Point(0, 0), (byte) 0);	
 	}
 
+	/* get input variable determined parameters */
 	private int get_spin_sep(int tail, double visible_distance) {
-		return 10;
+		return Math.max(tail, Player.LOWEST_LEN_OF_TAIL);
 	}
-
 	private int get_detector_sep(int tail, double visible_distance) {
 		return 4;
 	}
-
-	private double threshold(int tail, double visible_distance) {
+	private double isCrowdedThreshold(int tail, double visible_distance) {
 		return 2.5;
 	}
 
-	private int getOppositeDirection(Cell player_cell, Set<Cell> nearby_cells, byte memory) {
+	private int getOppositeDirection(Cell player_cell, Set<Cell> nearby_cells) {
 		Cell nearest = null;
 		double minDis = Double.MAX_VALUE;
 		for (Cell c : nearby_cells) {
@@ -96,15 +124,92 @@ public class Player implements slather.sim.Player {
 		Point pos = nearest.getPosition();
 		double vx = player_cell.getPosition().x - pos.x;
 		double vy = player_cell.getPosition().y - pos.y;
-		int arg = extractAngleFromVector(vx, vy);
+		int arg = (int) extractAngleFromVector(vx, vy);
+		return arg; //gen.nextInt(10) - 5;
+	}
+
+	private int getOppositeDirectionP(Cell player_cell, Set<Pherome> nearby_pheromes) {
+		Pherome nearest = null;
+		double minDis = Double.MAX_VALUE;
+		for (Pherome c : nearby_pheromes) {
+			if (c.distance(player_cell) < minDis && c.player != player_cell.player) {
+				minDis = c.distance(player_cell);
+				nearest = c;
+			}
+		}
+
+		Point pos = nearest.getPosition();
+		double vx = player_cell.getPosition().x - pos.x;
+		double vy = player_cell.getPosition().y - pos.y;
+		int arg = (int) extractAngleFromVector(vx, vy);
 		return arg + gen.nextInt(30) - 15;
 	}
 
 	private int spin(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, int seperation) {
-		return ((this.our_angel_range) / seperation + memory) % (this.our_angel_range);
+		return ((this.our_angle_range) / seperation + memory) % (this.our_angle_range);
 	}
 
-	private int detector(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, int seperation) {
+
+	private int spin(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, int seperation, boolean flag) {
+		if (flag == true)
+			return	((Player.ANGLE_RANGE / seperation) / Player.SCALE + memory) % 120 ;
+		else 
+			return ( (memory - (Player.ANGLE_RANGE / seperation) / Player.SCALE) % 120 + 120) %120 ;
+	}
+
+
+
+	private int findLargestGap(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
+
+		int count_pherome = nearby_pheromes.size();
+		for (Pherome nearby_pherome : nearby_pheromes) {
+			if (nearby_pherome.player == player_cell.player) count_pherome --;
+		}
+		if (count_pherome == 1 && nearby_cells.size() == 0) {
+			return getOppositeDirectionP(player_cell, nearby_pheromes);
+		} else if (count_pherome == 0 && nearby_cells.size() == 1) {
+			return getOppositeDirection(player_cell, nearby_cells);
+		}
+
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+		List<Integer> l = new ArrayList<>();
+
+		for (Cell nearby_cell : nearby_cells) {
+			double dy = nearby_cell.getPosition().y - player_cell.getPosition().y;
+			double dx = nearby_cell.getPosition().x - player_cell.getPosition().x;
+			l.add((int) extractAngleFromVector(dx, dy));
+		}
+		for (Pherome nearby_pherome : nearby_pheromes) {
+			if (nearby_pherome.player == player_cell.player) continue;
+			double dy = nearby_pherome.getPosition().y - player_cell.getPosition().y;
+			double dx = nearby_pherome.getPosition().x - player_cell.getPosition().x;
+			l.add((int) extractAngleFromVector(dx, dy));
+		}
+
+		Collections.sort(l);
+		for (int i=0 ; i<l.size() ; ++i) {
+			int gap = 0;
+			int direction = 0;
+
+			if (i == l.size()-1) {
+				gap = l.get(0) - l.get(i) + 120;
+				direction = (l.get(0) + l.get(i))/2;
+			} else {
+				gap = l.get(i+1) - l.get(i);
+				direction = (l.get(i+1) + l.get(i))/2;
+			}
+			if (gap > 60) direction = (direction + 60) % 120;
+			
+			int[] pair = {gap, direction};
+			pq.offer(pair);
+		}
+
+		int size = pq.size();
+
+		return pq.poll()[1];
+	}
+
+	private int densityRadar(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, int seperation) {
 		double[] direction = new double[seperation];
 
 		for (Cell nearby_cell : nearby_cells) {
@@ -136,13 +241,20 @@ public class Player implements slather.sim.Player {
 			if (sum <= 0) break;
 		}
 
-		return (index * this.our_angel_range / seperation + gen.nextInt(this.our_angel_range / seperation)) % this.our_angel_range;
+		return (index * this.our_angle_range / seperation + gen.nextInt(this.our_angle_range / seperation)) % this.our_angle_range;
 	}
 
-	private boolean isCrowded(Cell player_cell, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, double d_filter) {
-		return density(player_cell, nearby_cells, nearby_pheromes, d_filter) >= threshold(this.tail, this.visible_distance);
+	private int detector(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, int seperation) {
+		if (this.visible_distance < 3) {
+			// TODO: remove hardcode
+			return findLargestGap(player_cell, memory, nearby_cells, nearby_pheromes);
+		} else {
+			return densityRadar(player_cell, memory, nearby_cells, nearby_pheromes, seperation);
+		}
 	}
 
+
+	/* use a number to reflect how dense a cell's neighborhood is */
 	private double density(Cell player_cell, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, double d_filter) {
 		double weightSum = 0;
 		for (Cell nearby_cell : nearby_cells) {
@@ -157,15 +269,12 @@ public class Player implements slather.sim.Player {
 		}
 		return weightSum;
 	}
-
-	private double trans1(double a) {
-		return 1.0/(a+0.5);
+	private boolean isCrowded(Cell player_cell, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes, double d_filter) {
+		return density(player_cell, nearby_cells, nearby_pheromes, d_filter) >= isCrowdedThreshold(this.tail, this.visible_distance);
 	}
 
-	private double trans2(double a) {
-		return 1/Math.pow((0.01+a),2);
-	}
-
+	
+	/* check if a position is prohibited */
 	private boolean collides(Cell player_cell, Point vector, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
 		Iterator<Cell> cell_it = nearby_cells.iterator();
 		Point destination = player_cell.getPosition().move(vector);
@@ -185,21 +294,36 @@ public class Player implements slather.sim.Player {
 		return false;
 	}
 
+	/* Transform function to transform a number to different scale */
+	public double trans1(double a) {
+		return 1.0/(a+0.5);
+	}
+	public double trans2(double a) {
+		return 1/Math.pow((0.01+a),2);
+	}
+
+	/* transform between angle and unit vector */
+	// TODO: add scale
 	private Point extractVectorFromAngle(int arg) {
 		double theta = Math.toRadians((double) arg * Player.SCALE);
 		double dx = Cell.move_dist * Math.cos(theta);
 		double dy = Cell.move_dist * Math.sin(theta);
 		return new Point(dx, dy);
 	}
-
+	private double findLeastAbs(double x1, double x2, double x3) {
+		if (Math.abs(x1) < Math.abs(x2)) return Math.abs(x1) < Math.abs(x3) ? x1 : x3;
+		else return Math.abs(x2) < Math.abs(x3) ? x2 : x3;
+	}
 	private double extractRatioFromVector(double dx, double dy) {
-		double theta = Math.atan(dy/dx);
-		double ratio = (theta / (2 * Math.PI) + (dx > 0 ? (dy < 0 ? 1 : 0) : 0.5));
+		// dx_ and dy_ are used to find "wraps around" distance 
+		double dx_ = findLeastAbs(dx, dx+this.side, dx-this.side);
+		double dy_ = findLeastAbs(dy, dy+this.side, dy-this.side);
+		double theta = Math.atan(dy_/dx_);
+		double ratio = (theta / (2 * Math.PI) + (dx_ > 0 ? (dy_ < 0 ? 1 : 0) : 0.5));
 		return ratio;
 	}
-
-	private int extractAngleFromVector(double dx, double dy) {
-		return (int) (extractRatioFromVector(dx, dy) * this.our_angel_range);
+	private double extractAngleFromVector(double dx, double dy) {
+		return extractRatioFromVector(dx, dy) * this.our_angle_range;
 	}
 
 }
